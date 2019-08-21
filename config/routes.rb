9c5d5512a66a3ce80347dbefbfad3bbf6a81767c
vo duc loi <shjnvo89@gmail.com>
2019-08-21 12:20:22 +0700

    build html header, footer and content videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'videos#index'
+  get '/websocket', to: 'videos#websocket'
+  get '/share', to: 'videos#share'
 end
